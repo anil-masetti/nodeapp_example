e7c4f6733eaff72a308701b14e4250d04001e98e
anil-masetti <masettianilkumar.com>
2022-03-29 17:03:16 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require("express");  //Require dependencies
+
+const geo = require("geoip-lite");
+
+const PORT = 3000;   //Define the PORT
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.get("/", function (req, res) {
+  
+    const ip = "182.71.173.3";   //Extract the IP address from the request
+
+  if (!ip)
+    return res.status(500).json({
+      error: true,
+      message: "Sorry, we couldn't extract your IP address at the moment.",
+    });
+
+  let details = geo.lookup(ip);  //Extract details the from IP address
+  console.log(details.country,details.region,details.city)
+  return res.send({   
+          //Send the data to the client
+    country: details.country,
+    state: details.region,
+    city: details.city,
+    Ip:ip
+  });
+ 
 });
-app.listen(3000, function () {
-  console.log('Listening to Port 3000');
+  
+app.listen(PORT, () => {          // Start the server
+    console.log('Listening to Port 3000');
 });
+
